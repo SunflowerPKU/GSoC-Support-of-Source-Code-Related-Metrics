e946421cd3b3b5f9481fa981fa6d072daf58d77e
Xin Tan <746534561@qq.com>
2019-03-19 14:12:46 +0800

    Update test-github.py

--- Microtask 2/test-github.py
+++ Microtask 2/test-github.py
@@ -3,7 +3,7 @@
 from perceval.backends.core.github import GitHub
 
 
-repo = GitHub(owner='elastic', repository='logstash', api_token='61d317d94b4b93e654c1efcf0ffe68ffab95134e')
+repo = GitHub(owner='elastic', repository='logstash', api_token='***')
 # # fetch all issues and print each title
 for issue in repo.fetch():
     print(issue)
